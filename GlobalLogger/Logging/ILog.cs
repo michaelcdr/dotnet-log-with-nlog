d786b6254456aca9d491cd634ed7c2ddeeb7bfcd
Michael Costa dos Reis <michaelcdr@hotmail.com>
2020-11-03 20:50:51 -0300

    first commit

---
+++ GlobalLogger/Logging/ILog.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalLogger.Logging
+{
+    public interface ILog
+    {
+        void Information(string message);
+        void Warning(string message);
+        void Debug(string message);
+        void Error(string message);
+    }
+}
